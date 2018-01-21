1bbf615e4c77373acc57f50c64603a4dd29a45c9
Hossein Pourreza <hpourreza@uchicago.edu>
2018-01-20 21:48:48 -0600

    adding more comments and a missing solution

---
+++ IntrotoML/notebooks/solutions/load_iris_csv.py
@@ -0,0 +1,16 @@
+import sklearn.datasets
+import os
+import pandas as pd
+import numpy as np
+
+iris_path = os.path.join(sklearn.datasets.__path__[0], 'data', 'iris.csv')
+iris_df=pd.read_csv(iris_path)
+#give proper names to different columns
+iris_df.columns=['s_l','s_w','p_l','p_w','target']
+#print a few lines from DataFrame
+iris_df.head()
+
+#load training set as features and target classes
+X=np.array(iris_df.drop('target',axis=1))
+y=np.array(iris_df.target)
+X.shape,y.shape
